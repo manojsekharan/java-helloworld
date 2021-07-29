c8137ab32cb3d0b284245af6c6d8f0cfabaf62df
manoj <maninmanoj@gmail.com>
2021-07-29 06:07:57 +0000

    push to the poll scm

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,11 +3,5 @@ public class HelloWorld {
     System.out.println("Hello world!");
     System.out.println("Hello world!");
     System.out.println("Hello world!");
-    System.out.println("Hello world!");
-    System.out.println("Hello world!");
-    System.out.println("Hello world!");
-    System.out.println("Hello world!");
-    System.out.println("Hello world!");
-    System.out.println("Hello world!");
   }
 }
